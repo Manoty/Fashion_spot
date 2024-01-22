9cec563232dbe0f4ec04cbae03c6e1b9189a4161
manoty <kevinnoty21@gmail.com>
2024-01-22 11:10:44 +0300

    included password reset and password reset_done using djangos inbuilt view

--- auth/urls.py
+++ auth/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views as my_views
 from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
     path('register/', my_views.register, name='registration-url'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login-url'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout-url')
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout-url'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
 ]
--- fashion_spot/settings.py
+++ fashion_spot/settings.py
@@ -77,6 +77,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fashion_spot.wsgi.application'
 
 
+
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kevinnoty21@gmail.com'
+EMAIL_HOST_PASSWORD = '123456t'
+
+
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
